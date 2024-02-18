faef3289ce43bef4be25555be7ed95a80703f20d
Daucko <dauda.a.tijani@gmail.com>
2024-02-18 11:03:55 +0000

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState();
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
+    },
+    {
+      name: "Caleb",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
+    },
+    {
+      name: "Caleb",
+      role: "Director of Engr.",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
+    },
+    {
+      name: "Caleb",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg",
+    },
+    {
+      name: "Caleb",
+      role: "The Devops Guy",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
+    },
+    {
+      name: "Caleb",
+      role: "Senior",
+      img: "https://images.pexels.com/photos/1642161/pexels-photo-1642161.jpeg",
+    },
+  ]);
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <>
           <input
@@ -16,9 +49,19 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name="Caleb" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </>
       ) : (
         <p>You can not see the employees</p>
