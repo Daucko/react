e5f4a4142a7894d43faea7f8eacd50015fe42826
Daucko <dauda.a.tijani@gmail.com>
2024-02-18 14:19:05 +0000

    finish edit modal

--- src/App.js
+++ src/App.js
@@ -7,36 +7,58 @@ function App() {
   const [role, setRole] = useState();
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Caleb",
       role: "Developer",
       img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg",
     },
     {
+      id: 2,
+
       name: "Caleb",
       role: "Manager",
       img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
     },
     {
+      id: 3,
+
       name: "Caleb",
       role: "Director of Engr.",
       img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
     },
     {
+      id: 4,
+
       name: "Caleb",
       role: "Software Engineer",
       img: "https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg",
     },
     {
+      id: 5,
+
       name: "Caleb",
       role: "The Devops Guy",
       img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
     },
     {
+      id: 6,
+
       name: "Caleb",
       role: "Senior",
       img: "https://images.pexels.com/photos/1642161/pexels-photo-1642161.jpeg",
     },
   ]);
+
+  const updateEmployee = (id, newName, newRole) => {
+    const updatedEmployee = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployee);
+  };
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -51,13 +73,14 @@ function App() {
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              console.log(uuidv4());
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
