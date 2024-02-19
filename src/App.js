94ff9dfeff7c75d134b890c79d287c0e43405d27
Daucko <dauda.a.tijani@gmail.com>
2024-02-19 18:44:16 +0000

    past component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
 
 function App() {
   const [role, setRole] = useState();
@@ -59,6 +61,16 @@ function App() {
     setEmployees(updatedEmployee);
   };
 
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name,
+      role,
+      img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -73,6 +85,14 @@ function App() {
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
               return (
                 <Employee
                   key={employee.id}
@@ -80,11 +100,12 @@ function App() {
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
-                  updateEmployee={updateEmployee}
+                  editEmployee={editEmployee}
                 />
               );
             })}
           </div>
+          <AddEmployee newEmployee={newEmployee} />
         </>
       ) : (
         <p>You can not see the employees</p>
