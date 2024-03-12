0b23b8e51ffa80069169996d373497e39d57dfaa
Daucko <dauda.a.tijani@gmail.com>
2024-03-12 18:08:47 +0000

    styling

--- src/components/AddCustomer.js
+++ src/components/AddCustomer.js
@@ -2,41 +2,37 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function EditEmployee(props) {
-  const [name, setName] = useState(props.name);
-  const [role, setRole] = useState(props.role);
+export default function AddCustomer(props) {
+  const [name, setName] = useState("");
+  const [industry, setIndustry] = useState("");
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [show, setShow] = useState(props.show);
 
   return (
     <>
       <button
-        onClick={handleShow}
-        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+        onClick={props.toggleShow}
+        className="block m-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
       >
-        Update
+        + Add Customer
       </button>
 
       <Modal
-        show={show}
-        onHide={handleClose}
+        show={props.show}
+        onHide={props.toggleShow}
         backdrop="static"
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Update Employee</Modal.Title>
+          <Modal.Title>Add Customer</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              handleClose();
-              // console.log("hello from edit employee");
-              // console.log(props.id, name, role);
-              props.updateEmployee(props.id, name, role);
+              setName("");
+              setIndustry("");
+              props.newCustomer(name, industry);
             }}
             id="editmodal"
             className="w-full max-w-sm"
@@ -47,13 +43,14 @@ function EditEmployee(props) {
                   className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                   htmlFor="name"
                 >
-                  Full Name
+                  Name
                 </label>
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
+                  placeholder="Google"
                   type="text"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
@@ -64,18 +61,19 @@ function EditEmployee(props) {
               <div className="md:w-1/3">
                 <label
                   className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  htmlFor="role"
+                  htmlFor="industry"
                 >
-                  Role
+                  Industry
                 </label>
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  id="role"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="industry"
+                  placeholder="Computing"
                   type="text"
-                  value={role}
-                  onChange={(e) => setRole(e.target.value)}
+                  value={industry}
+                  onChange={(e) => setIndustry(e.target.value)}
                 />
               </div>
             </div>
@@ -84,20 +82,18 @@ function EditEmployee(props) {
         <Modal.Footer>
           <button
             className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-            onClick={handleClose}
+            onClick={props.toggleShow}
           >
             Close
           </button>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             form="editmodal"
           >
-            Update
+            Add
           </button>
         </Modal.Footer>
       </Modal>
     </>
   );
 }
-
-export default EditEmployee;
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,30 +1,83 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { baseUrl } from "../shared";
+import AddCustomer from "../components/AddCustomer";
 
 export default function Customers() {
   const [customers, setCustomers] = useState();
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow(!show);
+  }
+
   useEffect(() => {
     const url = baseUrl + "/api/customers/";
     fetch(url)
-      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
         setCustomers(data.customers);
       });
   }, []);
+
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + "/api/customers/";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        //assume the add was successful
+        //hide the modal
+        toggleShow();
+        //make sure the list updated appropriately
+        console.log(data);
+        if (data.customer.id) {
+          setCustomers([...customers, data.customer]);
+        } else {
+          setCustomers([...customers]);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <>
       <h1>Here are our customers: </h1>
       {customers
         ? customers.map((customer) => {
             return (
-              <p>
-                <Link to={"/customers/" + customer.id}>{customer.name}</Link>
-              </p>
+              <div className="m-2" key={customer.id}>
+                <Link to={"/customers/" + customer.id}>
+                  <button className="no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                    {customer.name}
+                  </button>
+                </Link>
+              </div>
             );
           })
         : null}
+      <AddCustomer
+        newCustomer={newCustomer}
+        show={show}
+        toggleShow={toggleShow}
+      />
     </>
   );
 }
